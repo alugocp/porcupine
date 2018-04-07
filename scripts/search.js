42c658930a554c8972fe7aa2d0e31712c52a2352
alugocp <alugocp@aim.com>
2018-04-07 17:04:35 -0400

    update

--- scripts/search.js
+++ scripts/search.js
@@ -5,26 +5,28 @@ function search(){
   clientPromise.then(client => {
     const db = client.service('mongodb', 'mongodb-atlas').db('Quills');
     client.login().then(()=>
-      client.executeFunction("search_quills",{name:name,lang:lang}).then((result)=>
+      client.executeFunction("search_quills",{name:name,lang:lang}).then((result) => {
         console.log(result)
-      )
+        populate_results([result]);
+      })
     ).catch(err => {
-      console.error(err);
+      console.error(err)
     });
   });
 }
 
 function populate_results(array){
   const results=$("#results");
-  array.foreach(function(current){
+  array.forEach(function(current){
 	   results.append(new_result(current));
   });
 }
 
 function new_result(obj){
 	var html=$("<div>");
-	html.append($(<span>"Name: "+obj.name</span>));
-	html.append($(<span><br>+"Lang: "+obj.lang</span>));
-	html.append($(<span><br> + "Purpose: " +obj.desc</span>));
-	html.append($(<span><br> + "Preview: " + obj.preview</span>));
+	html.append($("<span>Name: "+obj.name+"</span><br>"));
+	html.append($("<span>Lang: "+obj.lang+"</span><br>"));
+	html.append($("<span>Purpose: " +obj.desc+"</span><br>"));
+	html.append($("<span>Preview: " + obj.preview+"</span>"));
+  return html;
 }
