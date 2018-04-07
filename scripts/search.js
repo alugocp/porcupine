0c2218204d2cad84d464c355e72df8f270a30ee4
Jonathan Bertiz <jonathan.bertiz@gmail.com>
2018-04-07 12:52:14 -0700

    update

--- scripts/search.js
+++ scripts/search.js
@@ -13,10 +13,17 @@ function search(){
   });
 }
 
-function populate_results(){
+function populate_results(array){
   const results=$("#results");
-
+  array.foreach(function(current){
+	results.append(new_result(current));
+  })
 }
-function new_result(obj){
 
+function new_result(obj){
+	var html=$("<div>");
+	html.append($(<span>"Name: "+obj.name</span>));
+	html.append($(<span><br>+"Lang: "+obj.lang</span>));
+	html.append($(<span><br> + "Purpose: " +obj.desc</span>));
+	html.append($(<span><br> + "Preview: " + obj.preview</span>));
 }
