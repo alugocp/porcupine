b3c1cdf145edf1f9cfc3d1b1b63df91a8301d12b
Jonathan Bertiz <jonathan.bertiz@gmail.com>
2018-04-07 14:26:14 -0700

    update

--- scripts/setup.js
+++ scripts/setup.js
@@ -9,3 +9,19 @@ $(".header").ready(function(){
     })
   },"json");
 });
+
+ const name=$(".search-bar input[type=text]").val();
+  const lang=$(".languages").val();
+  const clientPromise = stitch.StitchClientFactory.create('porcupineapp-dcxhf');
+  clientPromise.then(client => {
+    const db = client.service('mongodb', 'mongodb-atlas').db('Quills');
+    client.login().then(()=>{
+	window.location.href="search.html";
+      client.executeFunction("search_quills",{name:name}).then((result) => {
+        console.log(result)
+        populate_results([result]);
+      })
+    }).catch(err => {
+      console.error(err)
+    });
+  });
