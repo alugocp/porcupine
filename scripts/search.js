f407994994f3579e1eef461ce3cbd8fe347d9c17
alugocp <alugocp@aim.com>
2018-04-07 16:44:09 -0400

    update

--- scripts/search.js
+++ scripts/search.js
@@ -17,10 +17,9 @@ function search(){
 function populate_results(array){
   const results=$("#results");
   array.foreach(function(current){
-	results.append(new_result(current));
-  })
+	   results.append(new_result(current));
+  });
 }
-<<<<<<< HEAD
 
 function new_result(obj){
 	var html=$("<div>");
@@ -28,8 +27,4 @@ function new_result(obj){
 	html.append($(<span><br>+"Lang: "+obj.lang</span>));
 	html.append($(<span><br> + "Purpose: " +obj.desc</span>));
 	html.append($(<span><br> + "Preview: " + obj.preview</span>));
-=======
-function new_result(obj){
-  //html.append($("<span>Name: "+obj.name+"</span><br>"))
->>>>>>> 53d8e9af0c219ac1ce113708ea5714406c8c8d05
 }
